d6abcb7eb5770504929934be90009ee9ec203561
Daniel Johansson <danielj@comhem.se>
2018-02-28 15:16:14 +0100

    Added UI methods

--- src/com/lexicon/ui/UI.java
+++ src/com/lexicon/ui/UI.java
@@ -2,12 +2,47 @@ package com.lexicon.ui;
 
 import com.lexicon.garage.Garage;
 import com.lexicon.garage.GarageHandler;
+import java.util.Scanner;
 
 public class UI {
     private GarageHandler gh = null;
 
     public UI(){
         //gh = new Garage(SIZE);
+
+        welcome();
+        createGarage();
+
+        while (true) {
+            listMainFunctions();
+
+            Scanner in = new Scanner(System.in);
+            int choice=in.nextInt();
+
+            if (choice==1) {
+                //list all parked vehicles
+                System.out.println("Parked vehicles");
+                //gh.listParkedVehicles();
+
+            }
+            else if (choice==2) {
+                //list specific parked vehicles
+                System.out.println("Specific Parked vehicles");
+            }
+            else if (choice==3) {
+                System.out.println("Park vehicle");
+            }
+            else if (choice==4) {
+                //unpark vehicle
+                System.out.println("Unpark vehicle");
+            }
+            else if (choice==5) {
+                System.out.println("Set max capacity");
+            }
+            else if (choice==6) {
+                System.out.println("Find vehicle");
+            }
+        }
     }
 
     public static void main(String[] args){
@@ -17,4 +52,32 @@ public class UI {
     public void setGh(GarageHandler gh){
         this.gh = gh;
     }
+
+    public static void welcome() {
+        System.out.println("Welcome to Garage 1.0");
+
+
+    }
+
+    public static void createGarage() {
+        System.out.println("Create garage: Input size");
+        Scanner in=new Scanner(System.in);
+        int size=in.nextInt();
+        Garage g=new Garage(size);
+
+        //add garage
+    }
+
+    public static void listMainFunctions() {
+        System.out.println("--Garage functions--");
+        System.out.println("1. List all parked vehicles");
+        System.out.println("2. List all vehicle types parked");
+        System.out.println("3. Park specific vehicle");
+        System.out.println("4. Unpark specific vehicle");
+        System.out.println("5. Set maximum capacity");
+        System.out.println("6. Find vehicle");
+
+    }
+    
+
 }
