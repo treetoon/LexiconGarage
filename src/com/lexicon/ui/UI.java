3c6e0ae3304d0c1af2ceba88b90ef545ca55949d
Daniel Johansson <danielj@comhem.se>
2018-02-28 16:50:13 +0100

    UI changes

--- src/com/lexicon/ui/UI.java
+++ src/com/lexicon/ui/UI.java
@@ -3,69 +3,63 @@ package com.lexicon.ui;
 import com.lexicon.garage.Garage;
 import com.lexicon.garage.GarageHandler;
 import java.util.Scanner;
+import com.lexicon.garage.Vehicle;
 
 public class UI {
     private GarageHandler gh = new GarageHandler();
 
     public UI(){
+        superMetod();
+    }
 
+    public static void main(String[] args){
+        UI start = new UI();
+    }
 
-        welcome();
+    public void createGarage() {
+        System.out.println("Write total parking spots: ");
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+
+        //gh.addGarage();
+    }
+
+    public void superMetod(){
+        System.out.println("Welcome to Garage 1.0");
         createGarage();
 
         while (true) {
             listMainFunctions();
 
             Scanner in = new Scanner(System.in);
-            int choice=in.nextInt();
+            int choice = in.nextInt();
 
-            if (choice == 1) {
-                //list all parked vehicles
-                System.out.println("Parked vehicles");
-                //gh.listParkedVehicles();
+            if (choice == 1) { //list all parked vehicles
+                System.out.println("Listing all parked vehicles: ");
 
             }
-            else if (choice==2) {
+            else if (choice == 2) {
                 //list specific parked vehicles
                 System.out.println("Specific Parked vehicles");
             }
             else if (choice==3) {
-                System.out.println("Park vehicle");
+                System.out.println("Input vehicle type");
+                in = new Scanner(System.in);
+                String s=in.next();
+
+
             }
-            else if (choice==4) {
+            else if (choice == 4) {
                 //unpark vehicle
                 System.out.println("Unpark vehicle");
             }
-            else if (choice==5) {
+            else if (choice == 5) {
                 System.out.println("Set max capacity");
             }
-            else if (choice==6) {
+            else if (choice == 6) {
                 System.out.println("Find vehicle");
             }
         }
-    }
-
-    public static void main(String[] args){
-        UI ui = new UI();
-    }
-
-    public void setGh(GarageHandler gh){
-        this.gh = gh;
-    }
-
-    public static void welcome() {
-        System.out.println("Welcome to Garage 1.0");
-
-
-    }
-
-    public void createGarage() {
-        System.out.println("Create garage: Input size");
-        Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        gh.addGarage(new Garage(size));
-
-        //add garage
     }
 
     public static void listMainFunctions() {
